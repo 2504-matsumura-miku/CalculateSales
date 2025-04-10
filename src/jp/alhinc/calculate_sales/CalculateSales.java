619194d491b1a260a2c06bd28d2e685681e61a78
matsumura_miku <matsumura.miku@alhinc.jp>
2025-04-10 16:50:44 +0900

    【売上集計課題】【売上集計課題】エラー処理3 (4/10 17:00)

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +24,10 @@ public class CalculateSales {
 	private static final String UNKNOWN_ERROR = "予期せぬエラーが発生しました";
 	private static final String FILE_NOT_EXIST = "支店定義ファイルが存在しません";
 	private static final String FILE_INVALID_FORMAT = "支店定義ファイルのフォーマットが不正です";
+	private static final String FILE_NOT_SERNO = "売上ファイル名が連番になっていません";
+	private static final String TOTAL_OVER_10DIGIT = "合計金額が10桁を超えました";
+	private static final String BRANCH_CODE_ERROR = "の支店コードが不正です";
+	private static final String FILE_FORMAT_ERROR = "のフォーマットが不正です";
 
 	/**
 	 * メインメソッド
@@ -35,6 +40,12 @@ public class CalculateSales {
 		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
 
+		// コマンドライン引数が1つ設定されているか
+		if (args.length != 1) {
+			System.out.println(UNKNOWN_ERROR);
+			return;
+		}
+
 		// 支店定義ファイル読み込み処理
 		if (!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
 			return;
@@ -42,16 +53,32 @@ public class CalculateSales {
 
 		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
 
-		//指定したパスに存在する全てのファイル(または、ディレクトリ)の情報を格納（処理内容2-1）
+		// 指定したパスに存在する全てのファイル(または、ディレクトリ)の情報を格納（処理内容2-1）
 		File[] files = new File(args[0]).listFiles();
 		List<File> rcdFiles = new ArrayList<>();
 
-		//フォルダの格納情報から売上ファイルを探す（処理内容2-1）
+		// フォルダの格納情報から売上ファイルを探す（処理内容2-1）
 		for (int i = 0; i < files.length; i++) {
 
-			//files[i].getName() でファイル名を獲得し、8桁かつ.rcdのファイルを保持（処理内容2-1）
+			// files[i].getName() でファイル名を獲得し、8桁かつ.rcdのファイルを保持（処理内容2-1）
 			if (files[i].getName().matches("^\\d{8}[.]rcd$")) {
 				rcdFiles.add(files[i]);
+
+			}
+		}
+		// 売上ファイルを昇順にそーと
+		Collections.sort(rcdFiles);
+
+		// 売上ファイルが連番か確認
+		for (int i = 0; i < rcdFiles.size() - 1; i++) {
+			// 比較する2つのファイル名の先頭から数字の8文字を切り出し、int型に変換
+			int former = Integer.parseInt((files[i].getName()).substring(0, 8));
+			int latter = Integer.parseInt((files[i + 1]).getName().substring(0, 8));
+
+			// 2つのファイル名の数字を比較
+			if ((latter - former) != 1) {
+				System.out.println(FILE_NOT_SERNO);
+				return;
 			}
 		}
 
@@ -74,10 +101,28 @@ public class CalculateSales {
 					rcdFilesSub.add(line);
 				}
 
+				// 売上ファイルの中身が3行以上の場合はエラー
+				if (rcdFilesSub.size() != 2) {
+					System.out.println(file.getName() + FILE_FORMAT_ERROR);
+					return;
+				}
+
+				// 支店コードが存在しない場合はエラー
+				if (!branchNames.containsKey(rcdFilesSub.get(0))) {
+					System.out.println(file.getName() + BRANCH_CODE_ERROR);
+					return;
+				}
+
 				// Mapに加算していく為、売上金額の型を変換
 				long fileSale = Long.parseLong(rcdFilesSub.get(1));
 				// 支店コードごとに売上金額を加算
 				Long saleAmount = branchSales.get(rcdFilesSub.get(0)) + fileSale;
+
+				if (saleAmount >= 10000000000L) {
+					System.out.println(TOTAL_OVER_10DIGIT);
+					return;
+				}
+
 				//加算した売上金額をMapに追加
 				branchSales.put(rcdFilesSub.get(0), saleAmount);
 			} catch (IOException e) {
